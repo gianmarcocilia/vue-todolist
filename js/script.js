aad97cf3a133ad499dc58fba8b592ef7837b48a2
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-11-15 15:02:41 +0100

    add: bonus 3

--- js/script.js
+++ js/script.js
@@ -2,18 +2,25 @@ const {createApp} = Vue;
 const app = createApp({
     data() {
         return {
-            item: "",
+            item: {
+                text: "",
+                done: false
+            },
             items: []
         }
     },
     methods: {
         addItem: function() {
-            this.items.push(this.item)
-            this.item = "";
+            this.items.push({...this.item})
+            this.item.text = "";
         },
         removeTodo: function(clickedIndex) {
     
             this.items.splice(clickedIndex, 1)
+        },
+        checkTodo: function(clickedIndex) {
+            console.log(this.item.done);
+            this.items[clickedIndex].done = !this.items[clickedIndex].done;
         }
     }
 
